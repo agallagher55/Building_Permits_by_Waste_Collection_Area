ec0ac344815447f45053dfbcf1b927608f9fcc4c
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2023-05-16 08:20:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,124 +1,124 @@
-"""
-March 2022
-Run-Time: ~20 minutes
-A. Gallagher
-Outputs: Excel file
-- Important to make sure output excel file has 8 Collection Areas. Sometimes esri geoprocessing tool
-gets stuck and needs to re-run. time.sleep() can help.
-"""
-
 import arcpy
 import os
 import logging
+import pandas as pd
 
 from time import sleep
 from datetime import datetime
 from configparser import ConfigParser
 
 arcpy.env.overwriteOutput = True
+arcpy.SetLogHistory(False)
 
 TODAY = datetime.today().date().strftime('%m%d%Y')
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-OUTPUT_EXCEL_NAME = "Building_Dwellings_Summarized_{}.xlsx".format(TODAY)
+OUTPUT_EXCEL_NAME = f"Building_Dwellings_Summarized_{TODAY}.xlsx"
+EXCEL_PATH = os.path.join(SCRIPT_DIR, OUTPUT_EXCEL_NAME)
+LOG_FILE = f"logs_{TODAY}.log"
+LOGGING_LEVEL = "debug"
 
-# Config Settings from .ini file
+# Config Settings from ini file
 parser = ConfigParser()
 parser.read("settings.ini")
 settings = parser['settings']
 
-SDE = settings.get("SDE")
+WORKING_DIR = settings.get("WORKING_DIR")
+SDE = settings.get("SERVER_SDE")
 
-# FEATURES
-BUILDING_POLYGONS_FEATURE = os.path.join(SDE, "SDEADM.BLD_building_polygon")
-WASTE_COLLECTION_AREAS_FEATURE = os.path.join(SDE, "SDEADM.ADM_solid_waste", "SDEADM.ADM_waste_coll_area")
-BUILDING_USE_TABLE = os.path.join(SDE, "SDEADM.BLD_BUILDING_USE")
+DWEL_UNITS_SQL = "SUM_DWEL_UNITS <= 6 And SUM_DWEL_UNITS <> 0"
 
 # Logger Settings
-LOG_FILE = "logs_{}.log".format(TODAY)
-
-logger = logging.getLogger("__name__")
-level = logging.DEBUG
-logger.setLevel(level)
-
 FORMATTER = logging.Formatter(
     '%(asctime)s | %(levelname)s | FUNCTION: %(funcName)s | Msgs: %(message)s', datefmt='%d-%b-%y %H:%M:%S'
 )
 
+levels = {
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+    "critical": logging.CRITICAL
+}
+level = levels[LOGGING_LEVEL.lower()]
+
 handler = logging.FileHandler(LOG_FILE)
 handler.setFormatter(FORMATTER)
+
+logger = logging.getLogger("__name__")
+logger.setLevel(level)
 logger.addHandler(handler)
 
 
-def report(dwellings_table, working_gdb):
-
-    output_table = os.path.join(working_gdb, "units_summary")
-
-    # Update Table to where 'Coll_Area is None' to include dwellings outside a collection areain report
-    print("\nUpdating {} with NO COLLECTION AREA zones...".format(dwellings_table))
-    with arcpy.da.UpdateCursor(dwellings_table, "COLL_AREA", "COLL_AREA IS NULL") as cursor:
-        for row in cursor:
-            row[0] = "N/A"
-            cursor.updateRow(row)
-
-    # Get summary of dwelling units per collection area
-    summary_fields = [["DWEL_UNITS", "SUM"]]
-    case_fields = ["COLL_AREA", "BL_ID"]
-    dwellings_summary = arcpy.Statistics_analysis(
-        in_table=dwellings_table,
-        out_table=output_table,
-        statistics_fields=summary_fields,
-        case_field=case_fields
-    )[0]
-    sleep(5)
-
-    print("Filtering results...")
-    dwel_units_sql = "SUM_DWEL_UNITS <= 6"
-    filtered_dwellings, count = arcpy.SelectLayerByAttribute_management(
-        in_layer_or_view=dwellings_summary,
-        selection_type="NEW_SELECTION",
-        where_clause=dwel_units_sql,
-        invert_where_clause=""
-    )
-    sleep(5)
-
-    if int(count) < 125000:
-        logger.warning("# of records in filtered dwelling units table: {} (Expected ~131k)".format(count))
-    else:
-        logger.info("# of records in filtered dwelling units table: {} (Expected ~131k)".format(count))
-
-    print("Exporting to excel...")
-    if os.path.exists(OUTPUT_EXCEL_NAME):
-        os.remove(OUTPUT_EXCEL_NAME)
-
-    arcpy.TableToExcel_conversion(
-        Input_Table=filtered_dwellings,
-        Output_Excel_File=OUTPUT_EXCEL_NAME,
-        Use_field_alias_as_column_header="ALIAS",
-        Use_domain_and_subtype_description="CODE"
-    )
-
-    return OUTPUT_EXCEL_NAME
-
-
-def dwelling_units():
+def summarize_units(dwellings_table) -> (pd.DataFrame, pd.DataFrame):
     """
-    - Main process. Run geospatial analysis
-    :return:
+    - Convert table to DataFrame and aggregate data by Collection Area, Number of Dwelling Units
+    - Filter out any buildings with more than 6 units
+    - Append a row to sum number of dwelling units
+
+    :param dwellings_table: Table with BL_ID, DWELLING UNITS, COLLECTION AREA data
+    :return: DataFrames - 1) Raw Data, 2) Aggregated by Collection Area Summary Table
     """
 
-    # Create GEODATABASE and Use for as workspace
-    working_gdb = arcpy.CreateFileGDB_management(
-        out_folder_path=SCRIPT_DIR,
-        out_name="temp_workspace.gdb"
-    )[0]  # This will overwrite any existing gdb, clearing any existing feature classes
+    print("\nSummarizing Units...")
+    case_fields = ["COLL_AREA", "BL_ID"]  # First aggregate data by these fields
+    dataframe_columns = ["BL_ID", "COLL_AREA", "DWEL_UNITS"]
+
+    # Convert table to Numpy Array in order to create DataFrame
+    np_array = arcpy.da.FeatureClassToNumPyArray(
+        dwellings_table,
+        field_names=dataframe_columns,  # Set columns of interest
+        null_value={"DWEL_UNITS": 0}  # Translate NULL values to zero
+    )
+    df = pd.DataFrame(np_array)
+
+    # Get SUM and COUNT of DWELLING UNITS, BL_IDs
+    print(f"\tGrouping by {', '.join(case_fields)}...")
+    df_summary_one = df.query("DWEL_UNITS <= 6").groupby(case_fields).agg({"DWEL_UNITS": "sum", "BL_ID": "count"})
+
+    print(f"\tGrouping by COLL_AREA")
+    df_final = df_summary_one.groupby("COLL_AREA").agg({"DWEL_UNITS": "sum", "BL_ID": "count"})
+
+    # Add sum row
+    df_final.loc["TOTAL"] = df_final.sum()
+
+    df_final.rename(
+        columns={"DWEL_UNITS": "SUM of DWELLING UNITS", "BL_ID": "BL_ID COUNT"},
+        index={"COLL_AREA": "COLLECTION AREA"},
+        inplace=True
+    )
+
+    print(df_final)
+
+    # Export DataFrames to csv
+    for df_info in (df_final, "df_final.csv"), (df_summary_one, "df_summary.csv"), (df, "df_original.csv"):
+        dataframe, file_name = df_info
+        dataframe.to_csv(file_name, index=False)
+
+    return df_final, df_summary_one
+
+
+def waste_analysis(workspace, building_polygons, building_use, collection_areas):
+    """
+    The waste_analysis function performs the following tasks:
+        1. Dissolves building polygons by BL_ID
+        2. Spatially joins dissolved building polygons with waste collection areas (one-to-one)
+        3. Joins Building Use table to spatially joined feature class (building polygon and waste area) on BL_ID field
+            - This is done to get the number of dwelling units per building, which is needed for calculating
+              total number of dwelling units in each collection area
+
+    :param workspace: Set the workspace for the script
+    :param building_polygons: Dissolve the building polygons
+    :param building_use: Join the building use table to the building polygons
+    :param collection_areas: Spatially join the building polygons with the waste collection areas
+    """
 
     # DISSOLVE BUILDING POLYGONS
-    arcpy.AddMessage("\nDissolving Building Polygons...")
-    logger.debug("Dissolving Building Polygons...")
+    print("\nDissolving Building Polygons...")
+    logger.debug("\nDissolving Building Polygons...")
     dissolved_building_polygons = arcpy.Dissolve_management(
-        in_features=BUILDING_POLYGONS_FEATURE,
-        out_feature_class=os.path.join(working_gdb, "dissolved_building_polygons"),
+        in_features=building_polygons,
+        out_feature_class=os.path.join(workspace, "dissolved_building_polygons"),
         dissolve_field=["BL_ID"],
         statistics_fields=[],
         multi_part="MULTI_PART",
@@ -126,12 +126,12 @@ def dwelling_units():
     )[0]
 
     # SPATIAL JOIN DISSOLVED BUILDING POLYGONS WITH WASTE COLLECTION AREAS
-    arcpy.AddMessage("\nSpatially Joining Building Polygons and Waste Collection Areas...")
-    logger.debug("Spatially Joining Building Polygons and Waste Collection Areas...")
+    print("\nSpatially Joining Building Polygons and Waste Collection Areas...")
+    logger.debug("\nSpatially Joining Building Polygons and Waste Collection Areas...")
     buildings_waste_areas = arcpy.SpatialJoin_analysis(
         target_features=dissolved_building_polygons,
-        join_features=WASTE_COLLECTION_AREAS_FEATURE,
-        out_feature_class=os.path.join(working_gdb, "buildings_w_waste_areas"),
+        join_features=collection_areas,
+        out_feature_class=os.path.join(workspace, "buildings_w_waste_areas"),
         join_operation="JOIN_ONE_TO_ONE",
         join_type="KEEP_ALL",
         match_option="INTERSECT",
@@ -140,49 +140,111 @@ def dwelling_units():
     )[0]
 
     # JOIN BUILDING USE TABLE TO BUILDING POLYGONS
-    arcpy.AddMessage("\nJoining Building Use Table to Building Polygons...")
+    print("\nJoining Building Use Table to Building Polygons...")
     building_polygons_w_bld_use = arcpy.JoinField_management(
         in_data=buildings_waste_areas,
         in_field="BL_ID",
-        join_table=BUILDING_USE_TABLE,
+        join_table=building_use,
         join_field="BL_ID",
         fields=[]
     )[0]
 
-    sleep(10)  # Ensure join geoprocessing tool is finished.
+    sleep(10)
 
-    # Export Joined Feature - may need for debug only
-    arcpy.AddMessage("\tExporting Joined Features...")
+    # TODO: Export Joined Feature (for debug only)
+    print("\tExporting Joined Features...")
     joined_feature = arcpy.FeatureClassToFeatureClass_conversion(
         in_features=building_polygons_w_bld_use,
-        out_path=working_gdb,
+        out_path=workspace,
         out_name="buildings_w_waste_areas_blduse",
         where_clause="Join_Count > 0"  # Dwel Units <=6
     )[0]
 
     joined_records_count = int(arcpy.GetCount_management(joined_feature)[0])
-    arcpy.AddMessage("\tNumber of Joined Records: {}. (Should be >100k)".format(joined_records_count))
+    print(f"\tNumber of Joined Records: {joined_records_count}. (Should be >100k)")
 
     if joined_records_count < 100000:
-        logger.warning("Number of Joined Records: {}. (Should be >100k)".format(joined_records_count))
+        logger.warning(f"\tNumber of Joined Records: {joined_records_count}. (Should be >100k)")
     else:
-        logger.info("Number of Joined Records: {}. (Should be >100k)".format(joined_records_count))
+        logger.debug(f"\tNumber of Joined Records: {joined_records_count}. (Should be >100k)")
 
-    return working_gdb, joined_feature
+    return joined_feature
 
 
 if __name__ == "__main__":
-    arcpy.env.overwriteOutput = True
 
-    # working_gdb = os.path.join(SCRIPT_DIR, "temp_workspace.gdb")
-    # dwelling_units = os.path.join(working_gdb, "buildings_w_waste_areas_blduse")
+    # FEATURES
+    BUILDING_POLYGONS_FEATURE = os.path.join(SDE, "SDEADM.BLD_building_polygon")
+    WASTE_COLLECTION_AREAS_FEATURE = os.path.join(SDE, "SDEADM.ADM_solid_waste", "SDEADM.ADM_waste_coll_area")
+    BUILDING_USE_TABLE = os.path.join(SDE, "SDEADM.BLD_BUILDING_USE")
+
+    logger.info("Preparing Workspace...")
+    
+    # Create GEODATABASE and Use for as workspace
+    working_gdb = arcpy.CreateFileGDB_management(
+        out_folder_path=SCRIPT_DIR,
+        out_name="temp_workspace.gdb"
+    )[0]  # This will overwrite any existing gdb, clearing any existing feature classes
+
+    # Local features
+    local_bld_poly = arcpy.FeatureClassToFeatureClass_conversion(
+        in_features=BUILDING_POLYGONS_FEATURE,
+        out_path=working_gdb,
+        out_name='BLD_building_polygon'
+    )[0]
+    local_bld_use = arcpy.TableToTable_conversion(
+        in_rows=BUILDING_USE_TABLE,
+        out_path=working_gdb,
+        out_name='BLD_BUILDING_USE'
+    )[0]
+    local_waste_areas = arcpy.FeatureClassToFeatureClass_conversion(
+        in_features=WASTE_COLLECTION_AREAS_FEATURE,
+        out_path=working_gdb,
+        out_name='ADM_waste_coll_area'
+    )[0]
 
     try:
-        working_gdb, dwelling_units = dwelling_units()
-        excel_output = report(dwelling_units, working_gdb)
+        waste_building_units = waste_analysis(
+            workspace=working_gdb,
+            building_polygons=local_bld_poly,
+            building_use=local_bld_use,
+            collection_areas=local_waste_areas
+        )
+
+        # SUMMARIZE ATTRIBUTES BY BL_ID, COLLECTION_AREA ON DWELLING UNITS
+        print("\nSummarizing Attributes...")
+        logger.debug("\nSummarizing Attributes...")
+
+        df_final, df_summarized = summarize_units(dwellings_table=waste_building_units)
+
+        summary_rows = len(df_summarized.index)
+        area_1_units = df_final.loc["AREA 1", "SUM of DWELLING UNITS"]
+
+        print(f"# of Summary Rows: {summary_rows}, Expected: ~130k")
+        print(f"# of Area 1 Units: {area_1_units}, Expected: ~34k")
+
+        if summary_rows < 130000:
+            logger.warning(f"# of Summary Rows: {summary_rows}, Expected: ~130k")
+        else:
+            logger.debug(f"# of Summary Rows: {summary_rows}, Expected: ~130k")
+
+        if area_1_units < 30000:
+            logger.warning(f"# of Area 1 Units: {area_1_units}, Expected: ~34k")
+        else:
+            logger.debug(f"# of Area 1 Units: {area_1_units}, Expected: ~34k")
+
+        # EXPORT TO EXCEL
+        if len(df_summarized.index) > 0:
+            print("\nExporting Report to Excel...")
+
+            with pd.ExcelWriter(OUTPUT_EXCEL_NAME) as writer:
+                df_final.to_excel(writer, sheet_name="final")
+                df_summarized.to_excel(writer, sheet_name="summary")
+
+            logger.debug("\nExported to Excel")
 
     except arcpy.ExecuteError:
-        msg = "ARCPY ERROR: {}".format(arcpy.GetMessages())
+        msg = f"ARCPY ERROR: {arcpy.GetMessages()}"
 
         logger.error(msg)
         print(msg)
